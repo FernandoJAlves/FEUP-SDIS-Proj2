0047ddd2da750b74e00dc2e78f6fab7d8738d63d
FernandoJAlves <up201605270@fe.up.pt>
2019-05-31 15:24:32 +0100

    Finished backup observer logic

--- src/dbs/chord/observers/protocols/BackupObserver.java
+++ src/dbs/chord/observers/protocols/BackupObserver.java
@@ -1,9 +1,17 @@
 package dbs.chord.observers.protocols;
 
+import java.io.IOException;
+
+import dbs.chord.NodeInfo;
 import dbs.chord.messages.ChordMessage;
 import dbs.chord.messages.protocol.BackupMessage;
 import dbs.chord.messages.protocol.BackupResponseMessage;
 import dbs.chord.observers.PermanentObserver;
+import dbs.filesystem.Configuration;
+import dbs.filesystem.FileManager;
+import dbs.filesystem.threads.ResultCode;
+import dbs.filesystem.threads.Writer;
+import dbs.network.SocketManager;
 
 public class BackupObserver extends PermanentObserver {
 
@@ -14,10 +22,27 @@ public class BackupObserver extends PermanentObserver {
     @Override
     public void notify(ChordMessage message) {
         assert message instanceof BackupMessage;
-        String s = new String(((BackupMessage) message).getFileContent());
-        System.out.println("DA BACKUP: " + s);
-        //TODO: Funcao sandro
-        // SomeClass.get().handleBackup((BackupMessage) message);
+        BackupMessage backupMessage = (BackupMessage) message;
+
+        NodeInfo remoteNode = message.getSender();
+        
+        String fileKey = new String(backupMessage.getFileId().toByteArray());
+        Writer writer; 
+       
+        try {
+            writer = new Writer(fileKey, backupMessage.getFileContent(), Configuration.Operation.BACKUP);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    
+        FileManager.getInstance().getThreadpool().submit(writer);
+
+        //TODO: Handle writer response
+        BackupResponseMessage responseMessage = new BackupResponseMessage(backupMessage.getFileId(), ResultCode.OK);
+        SocketManager.get().sendMessage(remoteNode, responseMessage);
+
     }
 
     @Override
